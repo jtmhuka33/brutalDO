e47f2d237fc01b6efa95b210f5556b8a4767c510
jtmhuka33 <jtmhuka33@gmail.com>
2025-11-23 13:10:07 +0100

    more project init setup

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,13 @@
+import "../../global.css";
+import {Stack} from "expo-router";
+
+export default function RootLayout() {
+    return (
+        <Stack screenOptions={{
+            headerShown: false,
+            animation: 'slide_from_right'
+        }}>
+            <Stack.Screen name="index" options={{headerShown: false}}/>
+        </Stack>
+    )
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,20 @@
-import "../global.css";
-import { Stack } from "expo-router";
+import {Stack} from "expo-router";
+import "../global.css"
+import {useColorScheme} from "@/hooks/use-color-scheme"
+import {DarkTheme, DefaultTheme, ThemeProvider} from "@react-navigation/native";
+import {StatusBar} from "expo-status-bar";
+import {SafeAreaView} from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+    const colorScheme = useColorScheme();
+    return (
+        <SafeAreaView style={{flex: 1}}>
+            <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+                <Stack>
+                    <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+                </Stack>
+                <StatusBar style="auto"/>
+            </ThemeProvider>
+        </SafeAreaView>
+    )
 }
